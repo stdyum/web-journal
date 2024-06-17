661bff668a7e91c3fb99b717d8682587e4c8752f
likdan <likdan.company@gmail.com>
2024-06-17 23:30:10 +0300

    0.0.16

--- src/components/journal-cell/journal-cell.component.ts
+++ src/components/journal-cell/journal-cell.component.ts
@@ -1,9 +1,9 @@
-import { ChangeDetectionStrategy, Component, inject, input } from '@angular/core';
+import { ChangeDetectionStrategy, Component, effect, inject, input, signal } from '@angular/core';
 import { JournalCell, JournalMark } from '../../models/journal';
 import { MarkDialogComponent } from '../../dialogs/mark-dialog/mark-dialog.component';
 import { MatDialog } from '@angular/material/dialog';
 import { JournalCellService } from './journal-cell.service';
-import { filter, map, switchMap, take } from 'rxjs';
+import { filter, map, Observable, switchMap, take, tap } from 'rxjs';
 
 @Component({
   selector: 'journal-cell',
@@ -20,8 +20,16 @@ export class JournalCellComponent {
   cell = input<JournalCell | null>(null);
   canEditMarks = input<boolean>(false);
 
+  marks = signal<JournalMark[]>([]);
+
   private service = inject(JournalCellService);
   private dialog = inject(MatDialog);
+
+  constructor() {
+    effect(() => this.marks.set(this.cell()?.marks ?? []), {
+      allowSignalWrites: true,
+    });
+  }
 
   addMark(cell: JournalCell): void {
     this.dialog.open(MarkDialogComponent)
@@ -34,22 +42,36 @@ export class JournalCellComponent {
         lessonId: cell.point.columnId,
       })))
       .pipe(take(1))
+      .pipe(tap(m => this.marks.update(marks => [...marks, m])))
       .subscribe();
   }
 
-  editMark(cell: JournalCell, mark: JournalMark): void {
+  showMarkDialog(cell: JournalCell, mark: JournalMark): void {
     this.dialog.open(MarkDialogComponent, { data: mark })
       .afterClosed()
       .pipe(filter(v => !!v))
       .pipe(switchMap(v => v.type === 'remove' ?
-        this.service.removeMark(mark.id, cell.point.columnId) :
-        this.service.editMark({
-          id: mark.id,
-          mark: v.value.mark,
-          studentId: cell.point.rowId,
-          lessonId: cell.point.columnId,
-        })))
+        this.removeMark(cell, mark) :
+        this.editMark(cell, mark, v.value.mark)))
       .pipe(take(1))
       .subscribe();
+  }
+
+  editMark(cell: JournalCell, mark: JournalMark, newMark: string): Observable<void> {
+    return this.service.editMark({
+      id: mark.id,
+      mark: newMark,
+      studentId: cell.point.rowId,
+      lessonId: cell.point.columnId,
+    })
+      .pipe(tap(() => this.marks.update(marks => {
+        marks[marks.indexOf(mark)].mark = newMark;
+        return [...marks];
+      })));
+  }
+
+  removeMark(cell: JournalCell, mark: JournalMark): Observable<void> {
+    return this.service.removeMark(mark.id, cell.point.columnId)
+      .pipe(tap(() => this.marks.update(marks => marks.filter(m => m.id !== mark.id))));
   }
 }
